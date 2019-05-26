2a278e33641975fbfa954e4bf217e2c3a1eb53b4
Shilin Victor <chrono.monochrome@gmail.com>
2019-05-27 01:38:53 +0300

    Update gulp to latest

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@ var gulp        = require('gulp'),
     uglify      = require('gulp-uglify');
 
 
-gulp.task('script', function() {
+gulp.task('script', gulp.parallel(function() {
     return gulp.src('script/*.js')
         .pipe(concat('demo.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('app/static/js'))
-});
+}));
 
-gulp.task('vendor', function() {
+gulp.task('vendor', gulp.parallel(function() {
     return gulp.src('vendor/*.js')
         .pipe(gulp.dest('app/static/js'))
-});
+}));
 
-gulp.task('nunjucks', () =>
+gulp.task('nunjucks', gulp.parallel(() =>
 	gulp.src('app/templates/fragments/*')
 		.pipe(nunjucks.precompile())
     .pipe(concat('templates.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/static/js'))
-);
+));
 
 
-gulp.task('default', ['build']); // Have gulp run the 'build' task as a default
+gulp.task('build', gulp.parallel(['script', 'vendor', 'nunjucks']));
+gulp.task('default', gulp.parallel(['build'])); // Have gulp run the 'build' task as a default
 
-gulp.task('build', ['script', 'vendor', 'nunjucks']);
