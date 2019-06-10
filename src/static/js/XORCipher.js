9e2d9473d56411e03223ed3f4238fe24a96c86b2
Shilin Victor <chrono.monochrome@gmail.com>
2019-06-10 17:03:05 +0300

    Fix MOZILLA_COND_OF_USE

--- src/static/js/XORCipher.js
+++ src/static/js/XORCipher.js
@@ -19,7 +19,7 @@
 //
 // This program is free software. It comes without any warranty, to
 // the extent permitted by applicable law. You can redistribute it
-// and/or modify it under the terms of the Do What The Fuck You Want
+// and/or modify it under the terms of the Do What The **** You Want
 // To Public License, Version 2, as published by Sam Hocevar. See
 // http://www.wtfpl.net/ for more details.
 
