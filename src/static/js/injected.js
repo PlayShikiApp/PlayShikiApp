fdb27d4fa2b1a1d0c6d148dcf3a01bc84a4bdf9d
Shilin Victor <chrono.monochrome@gmail.com>
2019-06-16 15:58:03 +0300

    Actually fix watch button injection

--- src/static/js/injected.js
+++ src/static/js/injected.js
@@ -5,12 +5,12 @@ mainObserver.observe(document, observerConfig);
 
 function start() {
   var infoSection = document.querySelector('body#animes_show .c-info-right');
-  var watchLink = document.querySelector('#watchButton');
   var placeHolder = document.querySelector(".watch-online-placeholer");
+  var desc = document.querySelector(".c-description");
 
   //console.log("start");
 
-  if (infoSection === null || placeHolder === null || watchLink !== null)
+  if (infoSection === null || placeHolder === null || desc === null)
     return;
 
   var watched_episodes = 1;
@@ -19,6 +19,7 @@ function start() {
 
   if (watchedEl !== null) {
 	  var watched_episodes = parseInt(watchedEl.innerText) + 1;
+	  console.log("watched_episodes = " + watched_episodes);
   }
 
   var total_episodes = parseInt(placeHolder.getAttribute("data-total_episodes"));
@@ -26,16 +27,28 @@ function start() {
   var anime_id = location.pathname.split("-")[0].split("/")[2].replace(/\D/g, "");
   var loc = chrome.runtime.getURL("index.html") + "#/?anime_id="+ anime_id + "&episode=" + (watched_episodes > total_episodes ? 1 : watched_episodes);
 
-  var WatchButtonElement = document.createElement('div');
-  WatchButtonElement.classList.add('block');
-  WatchButtonElement.innerHTML = `
-	<a class="b-link_button dark watch-online" id="watchButton" href="#">Смотреть онлайн</a>
-  `;
+  var watchLink = document.querySelector('#watchButton');
 
-  infoSection.appendChild(WatchButtonElement);
+  if (watchLink === null) {
+	  var WatchButtonElement = document.createElement('div');
+	  WatchButtonElement.classList.add('block');
+	  WatchButtonElement.innerHTML = `
+		<a class="b-link_button dark watch-online" id="watchButton" href="#">Смотреть онлайн</a>
+	  `;
 
-  var WatchLink = WatchButtonElement.querySelector('#watchButton');
-  console.log(loc);
-  WatchLink.href = loc;
+	  infoSection.appendChild(WatchButtonElement);
+  }
+
+  var WatchLink = document.querySelector('#watchButton');
+  if (WatchLink !== null) {
+	  console.log(loc);
+	  WatchLink.href = loc;
+  }
+
+  if (desc !== null) {
+	console.dir(desc);
+	mainObserver.disconnect();
+	return;
+  }
 }
 
