fa1c936fadc317bf5e34e68f85a49034ffad3565
Shilin Victor <chrono.monochrome@gmail.com>
2019-08-26 04:37:39 +0300

    Block sibnet ads

---
+++ src/content/sibnet/js/main.js
@@ -0,0 +1,32 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	sendResponse({
+		ok: "ok"
+	});
+});
+
+function removeElements(selector) {
+	var number = 0;
+
+	document.querySelectorAll(selector).forEach(function(el) {
+		el.remove();
+		number++;
+	});
+
+	return number;
+}
+
+function start() {
+	//console.log("PlayShikiApp");
+
+	var numElements = removeElements("#vjs-overlayclip-box");
+	numElements += removeElements(".vjs-overlayclip-box-close");
+
+	if (numElements == 0)
+		setTimeout(start, 100);
+}
+
+try {
+	start();
+} catch(e) {
+	console.log(e);
+}
